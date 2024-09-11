80f242071107f9d6768f085e85fb4139d638a121
daFinndus <finnlucajensen555@gmail.com>
2024-09-11 23:59:31 +0200

    feat: Add build script

---
+++ backend/build.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const news = require("./routes/news");
+const measurement = require("./routes/measurement");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+// Serve the static files from the React app
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
+// Call other scripts
+app.use("/", news);
+app.use("/", measurement);
+
+if (!port) {
+  console.error("No port in environment variables found.");
+  process.exit(1);
+}
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}\n`);
+});
