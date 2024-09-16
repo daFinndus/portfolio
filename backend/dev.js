a975a222f9e277a75370a070f5b5e5e8207196a4
daFinndus <finnlucajensen555@gmail.com>
2024-09-16 10:51:39 +0200

    chore: Add spindown avoidance to dev.js

--- backend/dev.js
+++ backend/dev.js
@@ -22,3 +22,23 @@ if (!port) {
 app.listen(port, () => {
   console.log(`Server running from port: ${port}\n`);
 });
+
+// This function reloads the server to avoid renders spindown issue
+function reload(url) {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status ${response.status}`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+// Reload the server to avoid renders spindown issue
+setInterval(reload, 1000 * 30);
